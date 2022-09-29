70bcd530d03644ab5536c448a89f2d5c65498f85
Valeri <110026147+valeri7122@users.noreply.github.com>
2022-09-29 13:32:07 +0300

    Update HW9.py

--- HW9.py
+++ HW9.py
@@ -1,61 +1,61 @@
 users = {}
 
 def input_error(func):
-    def wrapper(m):
+    def wrapper(*args, **kwargs):
         while Exception:
             try:
-                func(m)     
+                func(*args, **kwargs)     
             except Exception: 
-                print(f'{m}')
+                print('Wrong enter. Repite please.')
             else:
                 break
     return wrapper
 
 @input_error
-def add(m):
+def add():
     a = input()
     a = a.split(' ')
     users.update({a[0]: a[1]})
 
 @input_error
-def change(m):
+def change():
     a = input()
     a = a.split(' ')
     users.update({a[0]: a[1]})
 
 @input_error
-def phone(m):
+def phone():
     a = input()
     print(users[a])
 
 @input_error
-def show_all(m):
+def show_all():
     for key, value in users.items():
         print(key, value)   
 
 @input_error
-def help(m):
-    return m
+def hello():
+    return 0
 
 @input_error
-def exit(m):
+def exit():
     return quit()   
 
-commands = {'add':(add,'Enter user name and phone-number: ','Give me name and phone please'),
-'good bye':(exit,'Good bye!',''),'close':(exit,'Good bye!',''),'exit':(exit,'Good bye!',''),
-'hello':(help,'How can I help you?',''),'show all':(show_all,'Users list:',''),
-'change':(change,'Enter user name and phone-number: ','Give me name and phone please'),
-'phone':(phone,'Enter user name: ','Give me name'),}
+commands = {'add':(add,'Enter user name and phone-number: '),
+'good bye':(exit,'Good bye!'),'close':(exit,'Good bye!'),'exit':(exit,'Good bye!'),
+'hello':(hello,'How can I help you?'),'show all':(show_all,'Users list:'),
+'change':(change,'Enter user name and phone-number: '),
+'phone':(phone,'Enter user name: ')}
 
 def main():
     while True:
-        input_string = input('Enter command: ' )
+        input_string = input('Enter command: ')
         input_string = input_string.lower()
 
         for el in commands:
             if input_string.find(el) != -1:
                 print(commands[el][1])
-                commands[el][0](commands[el][2])
+                commands[el][0]()
 
 
 if __name__ == '__main__':
