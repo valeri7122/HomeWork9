d3fb8252ced4707e7340e133526a68b1f7031b3e
Valeri <110026147+valeri7122@users.noreply.github.com>
2022-09-30 23:56:25 +0300

    Update HW9.py

--- HW9.py
+++ HW9.py
@@ -2,56 +2,69 @@ users = {}
 
 def input_error(func):
     def wrapper(*args, **kwargs):
-           try:
-               return func(*args, **kwargs)     
-           except Exception: 
-               print('Wrong enter. Repite please.')
+        try:
+            return func(*args, **kwargs)     
+        except KeyError:
+            print('Enter user name:')
+        except ValueError:
+            print('Enter correct type:')
+        except IndexError:
+            print('Give me name and phone please:')
     return wrapper
 
+
 @input_error
-def add():
-    a = input()
-    a = a.split(' ')
+def add(func_arg):
+    a = func_arg.split(' ')
     return users.update({a[0]: a[1]})
 
 @input_error
-def change():
-    a = input()
-    a = a.split(' ')
+def change(func_arg):
+    a = func_arg.split(' ')
     return users.update({a[0]: a[1]})
 
 @input_error
-def phone():
-    a = input()
-    print(users[a])
+def phone(func_arg):
+    p = users[func_arg]
+    print(p)
 
 @input_error
 def show_all():
+    print('Users list:')
     for key, value in users.items():
-        print(key, value)   
+        print(f'{key}:{value}')  
 
 @input_error
 def hello():
-    return 0
-
+    return 'How can I help you?'
+    
 @input_error
 def exit():
     return 'Good bye!'  
 
-commands = {'add':(add,'Enter user name and phone-number: '),
-'good bye':(exit,'Good bye!'),'close':(exit,'Good bye!'),'exit':(exit,'Good bye!'),
-'hello':(hello,'How can I help you?'),'show all':(show_all,'Users list:'),
-'change':(change,'Enter user name and phone-number: '),
-'phone':(phone,'Enter user name: ')}
+commands = {
+'add':add,
+'good bye':exit,
+'close':exit,
+'exit':exit,
+'hello':hello,
+'show all':show_all,
+'change':change,
+'phone':phone}
 
-@input_error
+
 def main():
-    
     while True:
-        input_string = input('Enter command: ').lower()
-        print(commands[input_string][1])
-        if commands[input_string][0]() == "Good bye!":
-            break
+        input_string = input('Enter command: ').strip().lower()
+        if input_string == 'add' or input_string == 'change':
+            commands[input_string](input('Enter user name and phone-number: ').strip().lower())
+        elif input_string == "phone":
+            commands[input_string](input('Enter user name: ').strip().lower())
+        elif input_string == "hello":
+            print(commands[input_string]())
+        elif commands[input_string]() == "Good bye!":
+            print(commands[input_string]())
+            break    
 
 
 if __name__ == '__main__':
